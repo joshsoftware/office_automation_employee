82a547bda9258613a44bd2db4962d0f4341ac21d
pranav <pranav@joshsoftware.com>
2014-02-25 16:55:18 +0530

    test cases are modified

--- spec/models/office_automation_employee/private_profile_spec.rb
+++ spec/models/office_automation_employee/private_profile_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 module OfficeAutomationEmployee
   describe PrivateProfile do
-    it { should have_fields(:pan_number, :personal_email, :passport_number, :qualification, :date_of_joining, :work_experience, :previous_company) }
-    it { should have_field(:date_of_joining).of_type(Date) }
-    it { should be_embedded_in(:user) }
-    it { validate_uniqueness_of(:pan_number) }
-    it { validate_uniqueness_of(:personal_email) }
-    it { validate_uniqueness_of(:passport_number) }
+    it { expect have_fields(:pan_number, :personal_email, :passport_number, :qualification, :date_of_joining, :work_experience, :previous_company) }
+    it { expect have_field(:date_of_joining).of_type(Date) }
+    it { expect be_embedded_in(:user) }
+    it { expect validate_uniqueness_of(:pan_number) }
+    it { expect validate_uniqueness_of(:personal_email) }
+    it { expect validate_uniqueness_of(:passport_number) }
   end
 end
--- spec/models/office_automation_employee/user_spec.rb
+++ spec/models/office_automation_employee/user_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 module OfficeAutomationEmployee
   describe User do
-    it { should have_fields(:email, :encrypted_password, :role, :status) }
-    it { should embed_one :public_profile }
-    it { should embed_one :private_profile }
-    it { should validate_presence_of(:role) }
-    it { should validate_presence_of(:email) }
+    it { expect have_fields(:email, :encrypted_password, :role, :status) }
+    it { expect embed_one :public_profile }
+    it { expect embed_one :private_profile }
+    it { expect validate_presence_of(:role) }
+    it { expect validate_presence_of(:email) }
   end
 end
