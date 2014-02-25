4ba12a3ca7fcf175e9f6c81e52706e2ce304b2bc
shwetakale <shwetakale13@gmail.com>
2014-02-25 16:57:13 +0530

    Test cases updated

--- spec/models/office_automation_employee/company_spec.rb
+++ spec/models/office_automation_employee/company_spec.rb
@@ -4,19 +4,19 @@ module OfficeAutomationEmployee
   describe Company do
 
     context 'It checks presence of fields' do
-      it { should have_fields(:name, :logo, :company_url) }
-      it { should have_field(:registration_date).of_type(Date) }
+      it { expect have_fields(:name, :logo, :company_url) }
+      it { expect have_field(:registration_date).of_type(Date) }
     end
 
     context 'It validates fields' do
-      it { should validate_presence_of(:name) }
-      it { should validate_uniqueness_of(:name) }
+      it { expect validate_presence_of(:name) }
+      it { expect validate_uniqueness_of(:name) }
     end
 
     context 'It checks for associations' do
-      it { should embed_one :registered_address }
-      it { should embed_one :current_address }
-      it { should have_and_belong_to_many :roles }
+      it { expect embed_one (:registered_address) }
+      it { expect embed_one (:current_address) }
+      it { expect have_and_belong_to_many (:roles) }
     end
 
   end
