083cbbd382a4af3828c840b30a9e9036352f74de
shwetakale <shwetakale13@gmail.com>
2014-02-27 13:09:08 +0530

    Seed data for role table added

--- app/models/office_automation_employee/role.rb
+++ app/models/office_automation_employee/role.rb
@@ -1,12 +1,20 @@
 module OfficeAutomationEmployee
   class Role
     include Mongoid::Document
- 
+
+    #Predifined roles which are added in db/seeds.rb
+    ADMIN = 'Admin'
+    HR = 'HR' 
+    EMPLOYEE = 'Employee'
+    ACCOUNTANT = 'Accountant'
+
+    ROLES = [ADMIN, HR, EMPLOYEE, ACCOUNTANT]
+
     field :name
 
     validates :name, presence: true
 
-    has_and_belongs_to_many :companies
-  
+    has_and_belongs_to_many :companies, class_name: 'OfficeAutomationEmployee::Company'
+
   end
 end
