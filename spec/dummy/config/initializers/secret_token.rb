8201247cf5d0a503d4cb0aeb181b3d7e8497ea8e
Kiran Chaudhari <kiran@joshsoftware.com>
2014-03-05 11:45:32 +0530

    Added mongoid.yml and secrate_token.rb

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '1c79a1cbb178abdf961016cbc8a6f8d7374883be2202bc9d75eb515335533f367c99d52e0b35d63865fbd03744c9be6932734f05cedc361576b64613dda252be'
