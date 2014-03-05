9008d50b29942a318a0fc2bc2c54419b6012e8c5
shwetakale <shwetakale13@gmail.com>
2014-03-05 16:03:34 +0530

    Secret token,mongoid.yml added

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+Dummy::Application.config.secret_key_base = 'a754738cc62d45c794d9eecdbbf7f6525b21b5d0fc2adad628d3f97bb1afb003744144e462a7c7020252a256296278663a6a654e230edddd0724bc6fccf2772'
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
