cc4fabd9e0b3d0d377481d3a81388583973a5722
shwetakale <shwetakale13@gmail.com>
2014-03-05 11:09:37 +0530

    Test cases updated

--- spec/controllers/office_automation_employee/registrations_controller_spec.rb
+++ spec/controllers/office_automation_employee/registrations_controller_spec.rb
@@ -14,7 +14,7 @@ module OfficeAutomationEmployee
 
     context "#create" do
 
-      it "will register a company, create user with role admin and assign roles to company" do
+      it "Registers a new company, creates new user with role admin and assigns roles to company" do
 
         post :create, user: { email: "abc@abc.com", password: "abcdabcd", password_confirmation: "abcdabcd", company_attributes: { name: "josh software",current_address: { address: "thube park", city: "pune", state: "maharashtra", country: "india", pincode: "411005", phone: 0202342323 },registered_address: {address: 'Pune', city: 'Pune', state: 'MH', country: 'India', pincode: '12345', phone: '0202342323'},same_as_registered_address: false}}   
 
@@ -29,7 +29,7 @@ module OfficeAutomationEmployee
         expect(response).to be_redirect
       end
       
-      it 'will check if same_as_registered_address is true then current address is same as registered address' do
+      it 'Checks if same_as_registered_address field is true then current address is same as registered address' do
 
         post :create, user: { email: "abc@abc.com", password: "abcdabcd", password_confirmation: "abcdabcd", company_attributes: { name: "josh software",current_address: { address: "pune", city: "pune", state: "MH", country: "india", pincode: "411005", phone: 0202342323 },registered_address: {address: 'Pune', city: 'Pune', state: 'MH', country: 'India', pincode: '12345', phone: '0202342323'},same_as_registered_address: true}}   
 
@@ -47,7 +47,7 @@ module OfficeAutomationEmployee
         expect(registered_address.pincode).to eq(current_address.pincode)
       end
 
-      it "will not create company if company with same name is aldready exist" do
+      it "Does not create company if company with same name is aldready exist" do
       
       Company.create(name: "josh software")
 
@@ -58,7 +58,7 @@ module OfficeAutomationEmployee
 
       end
 
-      it "will not create user if user with same email-id exist" do
+      it "Does not create user if user with same email-id exist" do
 
         post :create, user: { email: 'abc@abc.com', password: "abcdabcd", password_confirmation: "abcdabcd", company_attributes: { name: "josh software",current_address: { address: "pune", city: "pune", state: "MH", country: "india", pincode: "411005", phone: 0202342323 },registered_address: {address: 'Pune', city: 'Pune', state: 'MH', country: 'India', pincode: '12345', phone: '0202342323'},same_as_registered_address: true}}   
         post :create, user: { email: 'abc@abc.com', password: "abcdabcd", password_confirmation: "abcdabcd", company_attributes: { name: "josh software",current_address: { address: "pune", city: "pune", state: "MH", country: "india", pincode: "411005", phone: 0202342323 },registered_address: {address: 'Pune', city: 'Pune', state: 'MH', country: 'India', pincode: '12345', phone: '0202342323'},same_as_registered_address: true}}   
