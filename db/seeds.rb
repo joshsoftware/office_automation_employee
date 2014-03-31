3537a04222259bd8a013f4b8ca8fa2dc0cc9a85b
shwetakale <shwetakale13@gmail.com>
2014-03-31 12:33:55 +0530

    Rake task for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-load(Rails.root.join( '../../db', 'seeds', "#{Rails.env.downcase}.rb"))
+OfficeAutomationEmployee::Role::ROLES.each do |role|
+  OfficeAutomationEmployee::Role.find_or_create_by(name: role.humanize)
+end
